d0c77c56d57d1a70ecc61cf5e35ec274c4a9f428
“Sadia <Sadia.islam.badhon@gmail.com>
2019-10-30 21:53:34 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,21 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title)
-    @title = title
-  end
+  GENRES = []
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def initialize(title)
+    @title = title 
+puts "Flipping the page...wow, you read fast!"
   end
+  
+  
+  #def turn_page
+    #puts "Flipping the page...wow, you read fast!"
+
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
+
 
 end
